234422bafce88dc3087dd9462df6770e87f322ae
narcisabadea <narcy611@yahoo.com>
2019-10-04 23:20:00 +0300

    show own gifts

--- src/MyGifts.js
+++ src/MyGifts.js
@@ -2,11 +2,16 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
+import CardMedia from "@material-ui/core/CardMedia";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import GiftDetails from "./GiftDetails";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -24,179 +29,57 @@ const useStyles = makeStyles(theme => ({
 
 export default function MyGifts() {
   const classes = useStyles();
+  const [gifts] = useCollection(firebase.firestore().collection("gifts"));
 
   return (
     <div>
-      <Grid container spacing={3}>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
+      {gifts && (
+        <span>
+          <Grid container spacing={3}>
+            {gifts.docs.map((doc, index) => {
+              const userId = doc.data().userId;
+              if (userId === firebase.auth().currentUser.uid) {
+                return (
+                  <Grid item xl={4} lg={4} md={6} sm={6} xs={12} key={index}>
+                    <Card className={classes.card}>
+                      <Grid container spacing={3} style={{ margin: "10px" }}>
+                        <Link to="/personProfile" className="personProfile">
+                          <Grid item>
+                            <Avatar
+                              alt="Avatar"
+                              src={doc.data().userphotoUrl}
+                            />
+                          </Grid>
+                        </Link>
+                        <Grid item>{doc.data().giftName}</Grid>
+                      </Grid>
+                      <CardMedia
+                        className={classes.media}
+                        image={doc.data().giftUrl}
+                        style={{ margin: "7px" }}
+                      />
+                      <CardActions disableSpacing>
+                        <div>
+                          <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                          </IconButton>
+                          <div>1.233</div>
+                        </div>
+                        <div>
+                          <IconButton aria-label="add to favorites">
+                            <i className="material-icons">grade</i>
+                          </IconButton>
+                          <div>Wish it</div>
+                        </div>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                );
+              }
+            })}
           </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton
-                aria-label="add to favorites"
-                style={{ color: "red" }}
-              >
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton
-                aria-label="add to favorites"
-                style={{ color: "yellow" }}
-              >
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
-          </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
-          </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
-          </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
-          </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-        <Grid item xl={4} lg={4} md={6} sm={6} xs={12}>
-          <Grid container spacing={3} style={{ margin: "10px" }}>
-            <Link to="/personProfile" className="personProfile">
-              <Grid item>
-                <Avatar alt="Avatar" src="images/pic.jpg" />
-              </Grid>
-            </Link>
-            <Grid item>
-              <GiftDetails />
-            </Grid>
-          </Grid>
-          <div className="img-cover img1"></div>
-          <CardActions disableSpacing>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <div>1.233</div>
-            </div>
-            <div>
-              <IconButton aria-label="add to favorites">
-                <i className="material-icons">grade</i>
-              </IconButton>
-              <div>Wish it</div>
-            </div>
-          </CardActions>
-        </Grid>
-      </Grid>
+        </span>
+      )}
     </div>
   );
 }
